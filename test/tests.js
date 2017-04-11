1fd3763256c403b2ce12f43d33ed6e9276094aaa
George <george.circleci.com>
2017-04-11 09:15:10 -0700

    Adding testing files

---
+++ test/tests.js
@@ -0,0 +1,8 @@
+test( "timeElapsed should be Zero before we start the Timer", function() {
+    equal( 0, 0);
+});
+
+
+test( "timeElapsed should be Zero before we start the Timer", function() {
+    equal( 0, 0);
+});
