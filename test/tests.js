a2593d81d19500fb9da988bf3a386974429a2317
George <george.circleci.com>
2017-04-24 11:31:00 -0700

    Removed test to make this example more DIY

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,5 @@
 test( "timeElapsed should be Zero before we start the Timer", function() {
-    equal( 0, 0);
+    equal( 1, 0);
 });
 
 
-test( "Test that we can add feature and test it using CircleCI", function() {
-    equal( returnPlayerName(), "george");
-});
