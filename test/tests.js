9cb54be633f54f4d5b94039e37dd23139ecb408e
testAccount-234 <zypher452+testAccount234@gmail.com>
2017-04-27 13:27:00 -0700

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -2,4 +2,6 @@ test( "timeElapsed should be Zero before we start the Timer", function() {
     equal( 0, 0);
 });
 
-
+test( "Test that we can add feature and test it using CircleCI", function() {
+    equal( meaningOfLife(), "42");
+});
